68d28d5df7bec8375086c462f25d6b625a9c8da5
Jenny Dinh <jennythi.d@gmail.com>
2021-01-18 20:16:30 -0500

    add nav bar

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -2,6 +2,14 @@ const HomePage = () => {
     return (
         <div className="homepage__container">
             <span>.01</span>
+            <nav className="homepage__navbar">
+                <ul className="homepage__navbar-menu">
+                    <li>About Me</li>
+                    <li>My Experiences</li>
+                    <li>Projects I've built</li>
+                    <li>Contact Me</li>
+                </ul>
+            </nav>
             <div className="homepage__background">
                 <h1>Jenny <span>Dinh</span></h1>
             </div>
