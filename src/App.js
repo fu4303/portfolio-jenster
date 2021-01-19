29c628803ea2f8bb29534855cb6a2c7fe46d49f0
Jenny Dinh <jennythi.d@gmail.com>
2021-01-19 18:58:13 -0500

    adding skills section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import HomePage from './layout/HomePage'
+import Skills from './layout/Skills'
 import styles from './styles/styles.css'
 
 function App() {
   return (
     <div className="app">
       <HomePage />
+      <Skills />
     </div>
   );
 }
---
+++ src/layout/Skills.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const Skills = () => {
+    return(
+        <section className="skills__container">
+            <h1>Skills and Ability</h1>
+        </section>
+    )
+}
+
+export default Skills
